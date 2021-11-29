d1c4f1d8d90384c35b0f8c3931af99ae4ecf0020
MD <adlaon.mde@gmail.com>
2021-11-29 23:32:18 +0800

    Added meta tags and icon

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,19 @@
+import Head from "next/head";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>GTA Tracker</title>
+        <link rel="shortcut icon" href="/GTA_Logo.jpg" />
+        <meta
+          name="description"
+          content="Gamesports Talent Academy Scholar Tracker and Management"
+        />
+      </Head>
       <Header></Header>
       <main className="w-full">{children}</main>
       <Footer></Footer>
